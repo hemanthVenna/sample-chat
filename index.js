63e6acdf5e7fb07dec22334150c97ebcb9ad3124
surendra20 <surendranath.gurram@popcornapps.com>
2018-09-04 12:42:53 +0530

    api added

--- index.js
+++ index.js
@@ -12,42 +12,36 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
-// var io = require('socket.io')(http,{
-//   path: '/getData',
-//   serveClient: false,
-//   // below are engine.IO options
-//   pingInterval: 10000,
-//   pingTimeout: 5000,
-//   cookie: false
-// });
+var io = require('socket.io')(http,{
+  path: '/getData',
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
 
-var io = require('socket.io')(http);
-// io.path('/getData')
+// var io = require('socket.io')(http);
 var port = process.env.PORT || 5000;
 
+
 app.get('/getData', function(req,res){
-  console.log("get data response");
-  res.send('you are in getDAta');
-})
-
-
-// app.get('/getData', function(req,res){
-//   io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
-// });
+  io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+});
 app.get('/', function(req, res){
   console.log('req. comes')
   res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
