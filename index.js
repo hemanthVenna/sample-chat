b0a5b7edfcb17891043ecfef3a71d3d891f2232a
surendra20 <surendranath.gurram@popcornapps.com>
2018-09-04 12:56:54 +0530

    api added

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var app = require('express')();
-var cors = require('cors')
+// var cors = require('cors')
 var http = require('http').Server(app);
 
-app.use(function (req, res, next) {
-    var allowedOrigins = ['https://calm-meadow-51226.herokuapp.com/'];
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.use(function (req, res, next) {
+//     var allowedOrigins = ['https://calm-meadow-51226.herokuapp.com/'];
+//     var origin = req.headers.origin;
+//     if (allowedOrigins.indexOf(origin) > -1) {
+//         res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 var io = require('socket.io')(http,{
   path: '/getData',
   serveClient: false,
@@ -22,26 +22,26 @@ var io = require('socket.io')(http,{
 });
 
 // var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8000;
 
 
-app.get('/getData', function(req,res){
-  io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-});
+// app.get('/getData', function(req,res){
+//   io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+// });
 app.get('/', function(req, res){
   console.log('req. comes')
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
