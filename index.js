2ff7acda91566e6288827a57f07a309481aab249
surendra20 <surendranath.gurram@popcornapps.com>
2018-09-04 10:30:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var app = require('express')();
+var cors = require('cors')
+var http = require('http').Server(app);
+
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// var io = require('socket.io')(http,{
+//   path: '/getData',
+//   serveClient: false,
+//   // below are engine.IO options
+//   pingInterval: 10000,
+//   pingTimeout: 5000,
+//   cookie: false
+// });
+
+var io = require('socket.io')(http);
+// io.path('/getData')
+var port = 8000;
+
+app.get('/getData', function(req,res){
+  console.log("get data response");
+  res.send('you are in getDAta');
+})
+
+
+// app.get('/getData', function(req,res){
+//   io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+// });
+app.get('/', function(req, res){
+  console.log('req. comes')
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
